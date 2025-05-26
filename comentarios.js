6f0589ff92ae3d756ee7264c7588ccc815070978
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-26 23:50:33 +0000

    comentarios

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+var angulo = Math.PI * 3;
+console.log( 'seno(3PI) =' + Math.sin(angulo)) // Éste es un comentario de línea 
+
+var potencia = Math.pow(6,3);
+console.log( '6^3 =' + potencia) // este es otr comentario de línea 
+
+/**
+ * Este es un comentario multilinea 
+ */
