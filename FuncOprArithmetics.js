6306b60a668c315a37cf4bc501d4b24206b0c5d3
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-26 15:00:35 +0000

    actualizacion

--- FuncOprArithmetics.js
+++ FuncOprArithmetics.js
@@ -1,3 +1,16 @@
 /**
  * Sintaxis del lenguaje JavaScrib
  */
+
+let num1 = 8;
+let num2 = 6;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var producto = num1 * num2;
+var division = num1 / num2;
+
+console.log('El resultado de la suma es: ' + suma);
+console.log('El resultado de la resta es: ' + resta);
+console.log('El resultado del producto es: ' + producto);
+console.log('El resultado de la división es: ' + division);
