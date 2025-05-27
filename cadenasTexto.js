47c454067ebd89b0bda7f75d93e5e2d8a7f1411f
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-27 06:11:37 +0000

    cadenas texto

---
+++ cadenasTexto.js
@@ -0,0 +1,9 @@
+
+/*
+ * Ordenamiento de numeros como cadenas de texto
+ */
+
+var arg = [10,  53, 12, 56, 120];
+console.log(arg.join(', '));
+arg.sort();
+console.log(arg.join(', '));
--- registros.js
+++ registros.js
@@ -15,3 +15,12 @@ for(let i = 0; 1 < registros.length; i++){
         console.log( '     ' + registros[i][j] );
       }
 }
+
+/*
+ * Ordenamiento de numeros como cadenas de texto
+ */
+
+var arg = [10,  53, 12, 56, 120];
+console.log(arg.join(', '));
+arg.sort();
+console.log(arg.join(', '));
