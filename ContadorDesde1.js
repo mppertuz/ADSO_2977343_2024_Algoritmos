402a765e1f8017b9dc5895bada8696682b60c71d
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-27 02:29:50 +0000

    contador desde 1 hasta numero dado

---
+++ ContadorDesde1.js
@@ -0,0 +1,11 @@
+/**
+ * Sumar los números que desde 1 hasta el valor dado.
+ */
+
+const cantidad = 5;
+var suma = 0;
+
+    for (let i = 1; i <= cantidad; i++) {
+        suma += i;
+    }
+    console.log(suma);
