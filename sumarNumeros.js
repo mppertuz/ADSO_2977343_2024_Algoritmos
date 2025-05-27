cacedbb4558472baca91b39f45d85b69982fc585
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-27 02:16:27 +0000

    suma desde 1 hasta numeroN

---
+++ sumarNumeros.js
@@ -0,0 +1,11 @@
+
+/**
+ * se necesita elaborar un algoritmo en javascript que, 
+ * dado un número entero, sume todos los números naturales que hay hasta ese número.
+ * Por ejemplo, si el usuario digita 3, el programa debe sumar 1 + 2 + 3, 
+ * si el usuario digita 5 el programa debe sumar 1 + 2 + 3 + 4 + 5.
+ * Al finalizar, debe imprimir el resultado.
+ */
+
+
+
