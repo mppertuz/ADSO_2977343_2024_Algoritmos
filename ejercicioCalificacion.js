a7a327889a91b2aaa73489daf519c636a07f986a
mppertuz <99558410+mppertuz@users.noreply.github.com>
2025-05-27 00:48:21 +0000

    condicionales simples

---
+++ ejercicioCalificacion.js
@@ -0,0 +1,12 @@
+/**
+ * Algoritmo Calificación 
+ */
+
+const Calificación = 4;
+
+	if (Calificación >= 3){
+        console.log('Aprobó');
+    }
+    else{
+        console.log('Reprobó');
+    }
---
+++ oldOrYoung.js
@@ -0,0 +1,11 @@
+/**
+ * Añgoritmo para saber sí es o no mayor de edad 
+ */
+
+const edad = 41;
+
+	if (edad >= 18) {
+        console.log('Es mayor de edad')
+    }
+
+    // ejemplo de condicionales simples
